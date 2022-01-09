272e79d8dd2e2afe4172f761454b2a9aec850c92
Awal07 <awalamoo7@gmail.com>
2022-01-09 03:09:35 +0000

    headerStyling

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -16,6 +16,12 @@ export const Content = styled.div `
 
 export const LogoImg = styled.img`
     width:200px;
+
+    @media screen and (max-width:500px) {
+        width: 150px;
+    }
 `;
 
-export const TMDBLogoImg = styled.img ``;
+export const TMDBLogoImg = styled.img `
+    width:200px;
+`;
